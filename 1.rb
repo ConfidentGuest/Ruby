62fcee3eca1fdfacc77be857c799e4528aa3b0f3
Guest <berestov_02@mail.ru>
2020-11-01 01:01:30 +0400

    git commit 1.rb

--- 1.rb
+++ 1.rb
@@ -1,1 +1,33 @@
 puts "Hello world!"
+my_num = 11
+my_boolean = true 
+my_string = "Ruby"
+puts my_num
+puts my_boolean
+puts my_string
+#maths
+my_num = 1+1
+my_num1 = 2-1
+my_num2 = 2*3
+my_num3 = 10/2
+my_num4 = 2**5
+my_num5 = 15 % 4
+puts my_num
+puts my_num1
+puts my_num2
+puts my_num3
+puts my_num4
+puts my_num5
+=begin
+if
+print "What's up?"
+print "That's a lot of numbers"
+Then What's up?That's a lot of numbers (text in 1 line)
+=end
+puts "What's the length of this string? - 'it's in the numbers 11'"
+puts "it's in the numbers 11".length
+puts "it's in the numbers 11".reverse
+puts "hello".downcase
+puts "hello".upcase
+name = "Wh".downcase.reverse.upcase #methods
+puts name
